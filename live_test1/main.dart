d77c45d704729d9d79052cdbb74c84ce58a63f35
Mohammad Ali <ali17.pbl@gmail.com>
2024-09-27 23:30:02 +0600

    add

--- live_test1/main.dart
+++ live_test1/main.dart
@@ -1,3 +1,10 @@
+import 'first_live_test.dart';
+
 void main(){
+Car car = Car('Toyota', 'Corolla', 2015);
+print('Brand : ${car.brand}');
+print('Model : ${car.model}');
+print('Year : ${car.year}');
+print('Car Age :${car.carAge()}');
 
 }
